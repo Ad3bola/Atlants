a397a0701fac1131eda3041a9cc4f67ca340155e
Adebola Fagade <debolafagade90@gmail.com>
2024-01-29 13:50:04 -0500

    Initial Commit

---
+++ Atlants/AtlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlantsApp.swift
+//  Atlants
+//
+//  Created by Adebola Fagade on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AtlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
